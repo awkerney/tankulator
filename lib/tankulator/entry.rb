b52c8fd5fdc18aed1f33df9f8c0acf5781d53ecd
Adam Kerney <adam.w.kerney@gmail.com>
2014-04-25 13:58:47 -0600

    Initial commit

---
+++ lib/tankulator/entry.rb
@@ -0,0 +1,31 @@
+class Tankulator::Entry
+	attr_accessor :distance_travelled
+	attr_accessor :driver
+	attr_accessor :category
+
+	@distance_travelled
+	@odometer_start
+	@odometer_end
+	@driver
+	@category
+
+	def initialize(line)
+		pieces = line.split ','
+		@odometer_start = pieces[0].to_f
+		@odometer_end = pieces[1].to_f
+		@distance_travelled = @odometer_end - @odometer_start
+
+		if pieces[2].chomp!.eql?('b')
+			@category = 'b'
+			@driver = 'b'
+		else
+			categoryAndDriver = pieces[2].split(':')
+			@category = categoryAndDriver[0]
+			@driver = categoryAndDriver[1]
+		end
+	end
+
+	def to_s
+		"#{@distance_travelled} KM traveled by driver #{@driver}"
+	end
+end
---
+++ tankulator.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+	s.name = 'Tankulator'
+	s.version = '0.0.1'
+	s.date = %q{2014-04-24}
+	s.summary = 'Fast gas tank splitting'
+	s.description = 'A simple(ish) tool for splitting tanks of gas between multiple drivers'
+	s.authors = ['Adam Kerney']
+	s.homepage = 'https://github.com/awkerney/tanker'
+	s.files = ["lib/tankulator.rb", "lib/tankulator/gastank.rb", "lib/tankulator/entry.rb"]
+	s.require_paths = ["lib"]
+end
