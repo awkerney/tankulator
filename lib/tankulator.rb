b2fa0fca8019f91d5d7f4c7e243d4a831111f4af
Adam Kerney <adam.w.kerney@gmail.com>
2014-04-26 22:18:39 -0600

    Update tankulator.rb to reflect Mercenary usage better

--- lib/tankulator.rb
+++ lib/tankulator.rb
@@ -1,45 +1,54 @@
+# Require all of the Ruby files in the given directory. (Stolen from Jekyll's source code)
+#
+# path - The String relative path from here to the directory.
+#
+# Returns nothing.
+def require_all(path)
+  glob = File.join(File.dirname(__FILE__), path, '*.rb')
+  Dir[glob].each do |f|
+    require f
+  end
+end
+
+# Internal Requirements
+require 'tankulator/entry'
+require 'tankulator/gastank'
+
+require_all 'tankulator/parsers'
+
 module Tankulator
 	# Starts running everything
 	# Returns nothing
-	def self.run
-		require 'tankulator/entry'
-		require 'tankulator/gastank'
+	def self.run(args)
+		inputFileName = args[0]
 		
-		if ARGV.count == 0
-			puts "USAGE: tankulator <filename>"
-			#print 'Please enter the name of the file read from: '
-			#inputFileName = gets.chomp
-		else
-			inputFileName = ARGV[0]
-			
-			if File.exists?(inputFileName)
-				newTank = GasTank.new
-				puts 'Reading file input...'
+		if File.exists?(inputFileName)
+			newTank = GasTank.new
+			puts 'Reading file input...'
 
-				inputFile = File.open(inputFileName, 'r')
-				# Read in tank information
-				# the first four lines are as follows
-				newTank.start_date = inputFile.readline # start date
-				newTank.end_date = inputFile.readline # end date
-				newTank.cost = inputFile.readline.to_f # cost of tank
-				newTank.total_distance_confirmation = inputFile.readline # total distance
-				newTank.driver_aliases = inputFile.readline # make printing drivers a bit better
+			inputFile = File.open(inputFileName, 'r')
+			# Read in tank information
+			# the first four lines are as follows
+			newTank.start_date = inputFile.readline # start date
+			newTank.end_date = inputFile.readline # end date
+			newTank.cost = inputFile.readline.to_f # cost of tank
+			newTank.total_distance_confirmation = inputFile.readline # total distance
+			newTank.driver_aliases = inputFile.readline # make printing drivers a bit better
 
-				# read in the rest of the lines until we hit the end of the file
-				begin
-					while
-						newTank.add_entry(Entry.new(inputFile.readline))
-					end
-				rescue EOFError
-						#puts "All lines read"
+			# read in the rest of the lines until we hit the end of the file
+			begin
+				while
+					newTank.add_entry(Entry.new(inputFile.readline))
 				end
-
-				inputFile.close()
-				puts newTank
-				newTank.driver_summaries
-			else
-				puts 'That file does not exist. Exiting.'
+			rescue EOFError
+					#puts "All lines read"
 			end
+
+			inputFile.close()
+			puts newTank
+			newTank.driver_summaries
+		else
+			puts 'That file does not exist. Exiting.'
 		end
 	end
 end
